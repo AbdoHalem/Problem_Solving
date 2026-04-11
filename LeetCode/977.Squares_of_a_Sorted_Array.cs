04da9116b4d558cd2be1a7db36bda511fcf535c2
Abdo_Halem10 <abdohalem305@gmail.com>
2026-04-12 00:24:30 +0200

    Solving problem 977-Squares of a Sorted Array (Easy) on LeetCode

---
+++ LeetCode/977.Squares_of_a_Sorted_Array.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int[] SortedSquares(int[] nums) {
+        int[] res = new int[nums.Length];
+        // Two Pointers Pattern to swap
+        int left = 0, right = nums.Length - 1;
+        int index = right;
+        
+        while(right > left){
+            if(nums[left] * nums[left] > nums[right] * nums[right]){
+                res[index] = nums[left] * nums[left];
+                left++;
+            }
+            else{
+                res[index] = nums[right] * nums[right];
+                right--;
+            }
+            index--;
+        }
+        while(index >= 0){
+            res[index--] = nums[left] * nums[left];
+            left++;
+        }
+        return res;
+    }
+}
